9b508e5e32ad72e3b667b6d075e090b23063aa69
Maikyau Shehu <35256490+maikyaushehu@users.noreply.github.com>
2022-11-22 20:34:31 +0100

    Delete admin directory

--- admin/hide_status.php
+++
@@ -1,5 +0,0 @@
-<?php
-include("../connection.php");
-mysqli_query($con,"UPDATE `notification` SET `status`='hide' WHERE `id` ='".$_GET['id']."'");
-header("Location: notification.php");
-?>
--- admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include("../connection.php");
-
-unset($_SESSION['user_login']);
-header('Location:login.php');
-exit();
-?>
